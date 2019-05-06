70b54dae74516728fa616db9233bfb867be38df4
Joshua Seigler <joshua@seigler.net>
2019-05-06 10:56:38 -0400

    add source and issues links to footer, update meta info

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,9 @@
 import style from './index.css';
 
 export default function () {
-	return (
-		<footer class={ style.footer }>
-			<span>Made with </span>
-			<i class={ style.heart } />
-			<span> by <a href="https://github.com/seigler">Joshua Seigler</a></span>
-		</footer>
-	);
+  return (
+    <footer class={style.footer}>
+      <span><a href='https://borderlands.com/'>Official Borderlands website</a> - <a href='https://github.com/seigler/bl3skills.com'>Source Code</a> - <a href='https://github.com/seigler/bl3skills.com/issues'>Issues</a></span>
+    </footer>
+  );
 }
