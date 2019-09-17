fde0e7780b0deae310551e5225775e0b0dbb2c85
José Miguel Tajuelo Garrigós <josemigueltajuelo@gmail.com>
2019-09-17 19:20:00 +0200

    Added WIP Beastmaster tree. TODO texts, ranks and effects

--- src/pages/Beastmaster/skills.js
+++ src/pages/Beastmaster/skills.js
@@ -2,30 +2,379 @@ import SKILLS from '@constants/skills';
 
 /* eslint-disable quotes */
 const skills = {
-  "Tree One": {
+  "Stalker": {
     "0": {
-      "?": {
-        text: "?",
+      "Jabber Sidekick": {
+        text: "FL4K is joined by a loyal Jabber companion, armed with a Pistol. While accompanied by the Jabber, FL4K’s Movement Speed is increased. Hold F to issue an Attack Command, which will cause the Jabber to throw a Radiation Barrel at enemies.",
+        type: SKILLS.AUGMENT_DIAMOND,
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "Fade Away": {
+        text: "FL4K cloaks, turning invisible. FL4K can fire 3 shots while cloaked, and each shot is automatically a Critical Hit. While cloaked, FL4K has increased Movement Speed and Health Regeneration.",
         type: SKILLS.ACTION_SKILL,
         ranks: 0,
         effect: (rank, level) => `?`,
       },
     },
-  },
-  "Tree Two": {
-    "0": {
-      "?": {
+    "1": {
+      "TODO_1": {
         text: "?",
-        type: SKILLS.ACTION_SKILL,
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_3": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+    },
+    "2": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_CHEVRON,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_3": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_4": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+    },
+    "3": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_CHEVRON,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_3": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_4": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_DIAMOND,
+      },
+    },
+    "4": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_CHEVRON,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_3": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_4": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_DIAMOND,
+      },
+    },
+    "5": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_CHEVRON,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+    },
+    "6": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
         effect: (rank, level) => `?`,
       },
     },
   },
-  "Tree Three": {
+  "Master": {
     "0": {
-      "?": {
-        text: "?",
+      "Jabber Sidekick": {
+        text: "FL4K is joined by a loyal Jabber companion, armed with a Pistol. While accompanied by the Jabber, FL4K’s Movement Speed is increased. Hold F to issue an Attack Command, which will cause the Jabber to throw a Radiation Barrel at enemies.",
+        type: SKILLS.AUGMENT_DIAMOND,
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "Fade Away": {
+        text: "FL4K cloaks, turning invisible. FL4K can fire 3 shots while cloaked, and each shot is automatically a Critical Hit. While cloaked, FL4K has increased Movement Speed and Health Regeneration.",
         type: SKILLS.ACTION_SKILL,
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+    },
+    "1": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_3": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+    },
+    "2": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_CHEVRON,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_3": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_4": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+    },
+    "3": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_CHEVRON,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_3": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_4": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_DIAMOND,
+      },
+    },
+    "4": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_CHEVRON,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_3": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_4": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_DIAMOND,
+      },
+    },
+    "5": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_CHEVRON,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_3": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+    },
+    "6": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+    },
+  },
+  "Hunter": {
+    "0": {
+      "Jabber Sidekick": {
+        text: "FL4K is joined by a loyal Jabber companion, armed with a Pistol. While accompanied by the Jabber, FL4K’s Movement Speed is increased. Hold F to issue an Attack Command, which will cause the Jabber to throw a Radiation Barrel at enemies.",
+        type: SKILLS.AUGMENT_DIAMOND,
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "Fade Away": {
+        text: "FL4K cloaks, turning invisible. FL4K can fire 3 shots while cloaked, and each shot is automatically a Critical Hit. While cloaked, FL4K has increased Movement Speed and Health Regeneration.",
+        type: SKILLS.ACTION_SKILL,
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+    },
+    "1": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_3": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+    },
+    "2": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_CHEVRON,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_3": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_4": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+    },
+    "3": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_CHEVRON,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_3": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_DIAMOND,
+      },
+    },
+    "4": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_CHEVRON,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_3": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_4": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_DIAMOND,
+      },
+    },
+    "5": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+        type: SKILLS.AUGMENT_CHEVRON,
+      },
+      "TODO_2": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+      "TODO_3": {
+        text: "?",
+        ranks: 0,
+        effect: (rank, level) => `?`,
+      },
+    },
+    "6": {
+      "TODO_1": {
+        text: "?",
+        ranks: 0,
         effect: (rank, level) => `?`,
       },
     },
