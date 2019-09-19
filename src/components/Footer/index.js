c16c3e7f7f5643a919565b9d73ee70bd51ac7d55
Joshua Seigler <joshua@seigler.net>
2019-09-19 17:34:20 -0400

    Add Github shields to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import style from './index.css';
 export default function () {
   return (
     <footer class={style.footer}>
-      <span><a href='https://borderlands.com/'>Official Borderlands website</a> - <a href='https://github.com/seigler/bl3skills.com'>Source Code</a> - <a href='https://github.com/seigler/bl3skills.com/issues'>Issues</a></span>
+      <span><a href='https://borderlands.com/'>Official Borderlands website</a> - <a href='https://github.com/seigler/bl3skills.com'>Source Code</a> - <a href='https://github.com/seigler/bl3skills.com/labels/bug'><img alt='GitHub issues: bugs' src='https://img.shields.io/github/issues-raw/seigler/bl3skills.com/bug' /></a> <a href='https://github.com/seigler/bl3skills.com/labels/enhancement'><img alt='GitHub issues: enhancements' src='https://img.shields.io/github/issues-raw/seigler/bl3skills.com/enhancement' /></a> <img alt='GitHub contributors' src='https://img.shields.io/github/contributors/seigler/bl3skills.com' /></span>
     </footer>
   );
 }
